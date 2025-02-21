8544fd63afdfb9dc34409a16f2e1c77f98476ce1
shivaay <shivoodhanka@gmail.com>
2025-02-21 14:14:15 +0530

    are function changes this quit are body parts .

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,8 @@ function layout() {
   return (
     <div>
       <html>
-      <body>        <h1>this is layout this page</h1>
+      <body>
+        <h1>this is layout this page</h1>
       </body>
    </html>
   
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
 function page() {
+  const handerlayout = () => {
+    // function body
+  }
   return (
     <div>
       <h1> this is page 1</h1>
-      
     </div>
   )
 }
